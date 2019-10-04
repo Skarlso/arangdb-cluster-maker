2c56081786c37d78358b23bd53129bc5d995963a
Gergely Brautigam <skarlso777@gmail.com>
2019-10-04 07:03:54 +0200

    Added tests

---
+++ tests/single-server/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Sourcing waiter
+source ../../src/waiter.sh
+
+# Setup
+
+echo "Setting up db for testing..."
+kubectl apply -f single-server.yaml
+if [[ $? -ne 0 ]]; then
+    echo "Failed to apply config file"
+    return 1
+fi
+wait_for_deployment_ready_state "arango_deployment=single-server"
+
+# Test
+
+# get exposed port and ip
+# do a curl
+# do basic auth
+
+# If something fails... exit 1
+
+kubectl delete -f single-server.yaml
+
+return 0
